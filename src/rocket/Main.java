a5d91d97af7e8e2c168919eff4aab86c7606816f
Rozmarynowski <rozmarynowski_adam@onet.pl>
2018-12-27 18:05:24 +0100

    Created a simple GUI
    GUI to tests animation.

--- src/rocket/Main.java
+++ src/rocket/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("mainWindow.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 260, 361));
         primaryStage.show();
     }
 
