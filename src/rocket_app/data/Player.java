f3e09cad971923598ae7425bb93a1d3c3a226dae
Piotr Janus <30784285+Piotrek1697@users.noreply.github.com>
2019-01-07 22:53:21 +0100

    Update Player.java

--- src/rocket_app/data/Player.java
+++ src/rocket_app/data/Player.java
@@ -13,6 +13,18 @@ public class Player implements Comparable<Player>{
         rank = iterator++;
     }
 
+    public int getRank() {
+        return rank;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
     public void setRank(int rank) {
         this.rank = rank;
     }
